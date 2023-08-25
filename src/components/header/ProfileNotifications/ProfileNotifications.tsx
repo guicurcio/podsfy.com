baf8ee989b56f70e8fabea974ae311a451998d02
guicurcio <guidomaurocurcio@gmail.com>
2023-08-25 18:33:06 -0300

    ...

--- src/components/header/ProfileNotifications/ProfileNotifications.tsx
+++ src/components/header/ProfileNotifications/ProfileNotifications.tsx
@@ -1,23 +1,9 @@
-'use client';
+"use client";
 
-import TooltipIconButton from "components/home/TooltipIconButton"
-import {
-  Home,
-  Inbox,
-  LogOut,
-  Mail,
-  MessageSquare,
-  Settings
-} from "lucide-react"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from "ui/components/Dropdown"
-import mergeClasses from "utils/mergeClasses"
+import TooltipIconButton from "components/home/TooltipIconButton";
+import { Inbox } from "lucide-react";
+import Link from "next/link";
+import mergeClasses from "utils/mergeClasses";
 
 /**
  * Props for the ProfileNotifications component.
@@ -26,58 +12,24 @@ export interface ProfileNotificationsProps {
   /**
    * Custom class names passed to the root element.
    */
-  className?: string
+  className?: string;
 }
 
 /**
  * ProfileNotifications Component
  */
-const ProfileNotifications = ({
-  className,
-}: ProfileNotificationsProps) => (
+const ProfileNotifications = ({ className }: ProfileNotificationsProps) => (
   <div className={mergeClasses("self-center align-middle", className)}>
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <TooltipIconButton
-          className="h-[28px] self-center rounded-md px-2 align-middle text-[13px]"
-          Icon={Inbox}
-          tooltipContent="Notifications"
-        ></TooltipIconButton>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className="w-[200px] overflow-x-auto rounded-[4px] border-0 bg-[#1f1f23] font-visuelt text-[14px] font-normal text-white/50 shadow-3xl"
-        align="end"
-        forceMount
-      >
-        <DropdownMenuGroup className="font-normal text-white/50">
-          <DropdownMenuItem className="font-normal text-white/50">
-            <MessageSquare className="mr-3 h-4 w-4 self-center align-middle" />
-            <span className="h-4 self-center  align-middle ">Profile</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Home className="mr-3 h-4 w-4 self-center align-middle" />
-            <span className="h-4 self-center align-middle">Home</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings className="mr-3 h-4 w-4 self-center align-middle" />
-            <span className="h-4 self-center align-middle">Settings</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Mail className="mr-3 h-4 w-4 self-center align-middle" />
-            <span className="h-4 self-center align-middle">Contact Us</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator className="mx-[4px]" />
-        <DropdownMenuItem>
-          <LogOut className="mr-3 h-4 w-4 self-center align-middle" />
-          <span className="h-4 self-center align-middle">Log out</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Link href="/notifications">
+      <TooltipIconButton
+        className="h-[28px] self-center rounded-md px-2 align-middle text-[13px]"
+        Icon={Inbox}
+        tooltipContent="Notifications"
+      ></TooltipIconButton>
+    </Link>
   </div>
-)
+);
 
-ProfileNotifications.displayName = "ProfileNotifications"
-
+ProfileNotifications.displayName = "ProfileNotifications";
 
 export default ProfileNotifications;
